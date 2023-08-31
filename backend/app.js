7e8e9c902123f505eb9acfd419dc658073296570
wolfgang.w <wolfgangwang@hotmail.ca>
2023-08-31 17:06:42 -0400

    users page

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ var bodyParser = require("body-parser");
 
 var indexRouter = require("./routes/index");
 var authRouter = require("./routes/auth");
-var customersRouter = require("./routes/customers");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
-app.use("/customers", customersRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ frontend/src/pages/users.js
@@ -0,0 +1,133 @@
+import { useCallback, useMemo, useState, useEffect } from "react";
+import Head from "next/head";
+// import { subDays, subHours } from 'date-fns';
+import ArrowDownOnSquareIcon from "@heroicons/react/24/solid/ArrowDownOnSquareIcon";
+import ArrowUpOnSquareIcon from "@heroicons/react/24/solid/ArrowUpOnSquareIcon";
+import PlusIcon from "@heroicons/react/24/solid/PlusIcon";
+import { Box, Button, Container, Stack, SvgIcon, Typography } from "@mui/material";
+import { useSelection } from "src/hooks/use-selection";
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+import { UsersTable } from "src/sections/user/users-table";
+import { UsersSearch } from "src/sections/user/users-search";
+import { applyPagination } from "src/utils/apply-pagination";
+
+const now = new Date();
+
+const useUsers = (page, rowsPerPage, data) => {
+  return useMemo(() => {
+    data = data || [];
+    return applyPagination(data, page, rowsPerPage);
+  }, [page, rowsPerPage, data]);
+};
+
+const useUserIds = (users) => {
+  return useMemo(() => {
+    return users.map((user) => user.id);
+  }, [users]);
+};
+
+const Page = () => {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const users = useUsers(page, rowsPerPage, data);
+  const usersIds = useUserIds(users);
+  const usersSelection = useSelection(usersIds);
+
+  const handlePageChange = useCallback((event, value) => {
+    setPage(value);
+  }, []);
+
+  const handleRowsPerPageChange = useCallback((event) => {
+    setRowsPerPage(event.target.value);
+  }, []);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      // setIsFetching(true);
+      const response = await fetch("http://localhost:8000/users");
+      const resData = await response.json();
+      setData(resData);
+      // setIsFetching(false);
+    }
+    // setIsFetching(false);
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Users | FamilyAlbum</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="xl">
+          <Stack spacing={3}>
+            <Stack direction="row" justifyContent="space-between" spacing={4}>
+              <Stack spacing={1}>
+                <Typography variant="h4">Registered Users</Typography>
+                <Stack alignItems="center" direction="row" spacing={1}>
+                  <Button
+                    color="inherit"
+                    startIcon={
+                      <SvgIcon fontSize="small">
+                        <ArrowUpOnSquareIcon />
+                      </SvgIcon>
+                    }
+                  >
+                    Import
+                  </Button>
+                  <Button
+                    color="inherit"
+                    startIcon={
+                      <SvgIcon fontSize="small">
+                        <ArrowDownOnSquareIcon />
+                      </SvgIcon>
+                    }
+                  >
+                    Export
+                  </Button>
+                </Stack>
+              </Stack>
+              <div>
+                <Button
+                  startIcon={
+                    <SvgIcon fontSize="small">
+                      <PlusIcon />
+                    </SvgIcon>
+                  }
+                  variant="contained"
+                >
+                  Add
+                </Button>
+              </div>
+            </Stack>
+            <UsersSearch />
+            <UsersTable
+              count={data.length}
+              items={users}
+              onDeselectAll={usersSelection.handleDeselectAll}
+              onDeselectOne={usersSelection.handleDeselectOne}
+              onPageChange={handlePageChange}
+              onRowsPerPageChange={handleRowsPerPageChange}
+              onSelectAll={usersSelection.handleSelectAll}
+              onSelectOne={usersSelection.handleSelectOne}
+              page={page}
+              rowsPerPage={rowsPerPage}
+              selected={usersSelection.selected}
+            />
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
