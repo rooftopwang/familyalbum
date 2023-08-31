bdf344b54a6306d42e93372939ea0b675e8e274e
wolfgang.w <wolfgangwang@hotmail.ca>
2023-08-31 13:41:55 -0400

    GET customers

--- backend/routes/customers.js
+++ backend/routes/customers.js
@@ -1,11 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var { readData } = require("../data/util");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    var data = require('../database/data.json');
-    var customers = data.customers;
-    res.send(customers);
+router.get("/", async (req, res, next) => {
+  var data = await readData("data.json");
+  console.log(data);
+  var customers = data.customers;
+
+  res.send(customers);
 });
 
 module.exports = router;
