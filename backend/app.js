2acbd064948b2e1703ab858cada651301d4907da
wolfgang.w <wolfgangwang@hotmail.ca>
2023-09-04 20:36:54 -0400

    hosting react in express

--- backend/app.js
+++ backend/app.js
@@ -25,14 +25,16 @@ app.use((req, res, next) => {
 });
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
+
 // app.use(bodyParser.json());
 
 app.use("/", indexRouter);
