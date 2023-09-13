a1367f204ebeb8bc4c5ecf21ee0137117e028fb5
wolfgang.w <wolfgangwang@hotmail.ca>
2023-09-13 12:09:19 -0400

    misc

--- frontend/src/hooks/use-mocked-user.js
+++ frontend/src/hooks/use-mocked-user.js
@@ -2,9 +2,10 @@ export const useMockedUser = () => {
   // To get the user from the authContext, you can use
   // `const { user } = useAuth();`
   return {
-    id: '5e86809283e28b96d2d38537',
-    avatar: '/assets/avatars/avatar-anika-visser.png',
-    name: 'Anika Visser',
-    email: 'anika.visser@devias.io'
+    id: "5e86809283e28b96d2d38537",
+    avatar:
+      "https://firebasestorage.googleapis.com/v0/b/familyalbum-797db.appspot.com/o/avatars%2Favatar-carson-darrin.png?alt=media&token=be3d5df3-9581-4a96-8c16-c37c9b32712b",
+    name: "Wolfgnag Wang",
+    email: "wolfgangwang@hotmail.ca",
   };
 };
